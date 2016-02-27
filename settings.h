078525b301f32a986ef89e263a5f81ca522c06ee
Carlos Casarez <casarezc@berkeley.edu>
2016-02-26 16:29:55 -0800

    Zero encoders for single robot run tests

--- settings.h
+++ settings.h
@@ -24,39 +24,23 @@
 #define TELEM_INCLUDE "vr_telem.h"
 #define TELEMPACKFUNC(x) vrTelemGetData(x)
 
-//Left legs robot 1
-#define LEFT_1_PID_NUM       0       //PID module index is 0-3
-#define LEFT_1_ENC_NUM       0       //amsEnc module index is 0-3
-#define LEFT_1_ENC_FLIP      0       //"forward" normal for left
-#define LEFT_1_PWM_FLIP      0
-#define LEFT_1_TIH_CHAN      1       //tiH module index is 1-4
-#define AMS_ENC_OFFSET_0     15304
-
-//Right legs robot 1
-#define RIGHT_1_PID_NUM      1       //PID module index is 0-3
-#define RIGHT_1_ENC_NUM      1       //amsEnc module index is 0-3
-#define RIGHT_1_ENC_FLIP     1       //"forward" reversed for right
-#define RIGHT_1_PWM_FLIP     0
-#define RIGHT_1_TIH_CHAN     2       //tiH module index is 1-4
-#define AMS_ENC_OFFSET_1     10918
-
-//Left legs robot 2
-#define LEFT_2_PID_NUM       2       //PID module index is 0-3
-#define LEFT_2_ENC_NUM       2       //amsEnc module index is 0-3
-#define LEFT_2_ENC_FLIP      0       //"forward" normal for left
-#define LEFT_2_PWM_FLIP      0
-#define LEFT_2_TIH_CHAN      3       //tiH module index is 1-4
-#define AMS_ENC_OFFSET_2     0
-
-//Right legs robot 2
-#define RIGHT_2_PID_NUM      3       //PID module index is 0-3
-#define RIGHT_2_ENC_NUM      3       //amsEnc module index is 0-3
-#define RIGHT_2_ENC_FLIP     1       //"forward" reversed for right
-#define RIGHT_2_PWM_FLIP     0
-#define RIGHT_2_TIH_CHAN     4       //tiH module index is 1-4
-#define AMS_ENC_OFFSET_3     0
+// Encoders Setup
+//Left legs
+#define LEFT_LEGS_PID_NUM       0       //PID module index is 0-3
+#define LEFT_LEGS_ENC_NUM       0       //amsEnc module index is 0-3
+#define AMS_ENC_OFFSET_0        0
+#define LEFT_LEGS_ENC_FLIP      0       //"forward" reversed for left
+#define LEFT_LEGS_PWM_FLIP      0
+#define LEFT_LEGS_TIH_CHAN      1       //tiH module index is 1-4
+//Right legs
+#define RIGHT_LEGS_PID_NUM      1       //PID module index is 0-3
+#define RIGHT_LEGS_ENC_NUM      1       //amsEnc module index is 0-3
+#define AMS_ENC_OFFSET_1        0
+#define RIGHT_LEGS_ENC_FLIP     1       //"forward" regular for right
+#define RIGHT_LEGS_PWM_FLIP     0
+#define RIGHT_LEGS_TIH_CHAN     2       //tiH module index is 1-4
 
 // Encoder number definition
-#define NUM_ENC 4
+#define NUM_ENC 2
 
 #endif //_SETTINGS_H
