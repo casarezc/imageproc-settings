df365fc2920d78bc9c8c6217013cdf438fa4ab2d
Carlos Casarez <casarezc@berkeley.edu>
2016-09-13 18:18:43 -0700

    changed settings for testing controller stability

--- settings.h
+++ settings.h
@@ -29,7 +29,7 @@
 #define LEFT_1_ENC_NUM       0       //amsEnc module index is 0-3
 #define LEFT_1_ENC_FLIP      0       //"forward" normal for left
 #define LEFT_1_PWM_FLIP      0
-#define LEFT_1_TIH_CHAN      1       //tiH module index is 1-4
+#define LEFT_1_TIH_CHAN      2       //tiH module index is 1-4
 #define AMS_ENC_OFFSET_0     13352
 
 //Right legs robot 1
@@ -37,7 +37,7 @@
 #define RIGHT_1_ENC_NUM      1       //amsEnc module index is 0-3
 #define RIGHT_1_ENC_FLIP     1       //"forward" reversed for right
 #define RIGHT_1_PWM_FLIP     0
-#define RIGHT_1_TIH_CHAN     2       //tiH module index is 1-4
+#define RIGHT_1_TIH_CHAN     1       //tiH module index is 1-4
 #define AMS_ENC_OFFSET_1     15637
 
 //Left legs robot 2
