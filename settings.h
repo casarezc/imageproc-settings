631ad35c7f2999fa96032af4cb15384feed375dc
Carlos Casarez <casarezc@berkeley.edu>
2017-12-15 15:18:41 -0800

    Reset Hall tail zero constants after dropping robot and regluing board. This robot also now has counterweights attached to cranks

--- settings.h
+++ settings.h
@@ -52,7 +52,7 @@
 #define TAIL_ZERO_INPUT   _RB3
 
 //Tail zero values
-#define TAIL_ZERO_POS 4519
-#define TAIL_ZERO_NEG -6269
+#define TAIL_ZERO_POS 4145
+#define TAIL_ZERO_NEG -6279
 
 #endif //_SETTINGS_H
