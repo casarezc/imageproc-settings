1c19d90d5fd343122c976c9fec6359c3941c7375
Carlos Casarez <casarezc@berkeley.edu>
2018-01-09 17:55:51 -0800

    Updated tail zero constants for roach 0x2162 (now has black foam, spray painted black shell, and TP 4014 leg mounts as defining features)

--- settings.h
+++ settings.h
@@ -52,7 +52,7 @@
 #define TAIL_ZERO_INPUT   _RB3
 
 //Tail zero values
-#define TAIL_ZERO_POS 4145
-#define TAIL_ZERO_NEG -6279
+#define TAIL_ZERO_POS 3989
+#define TAIL_ZERO_NEG -6331
 
 #endif //_SETTINGS_H
